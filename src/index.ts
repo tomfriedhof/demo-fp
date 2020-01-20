bd9bd0bd94c58261e6eb2739f5384b0c743d46f8
Tom Friedhof <tom@activelamp.com>
2020-01-20 13:46:00 -0700

    only using pure functions to update UI

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import * as App from './pure-function';
 
 const calc = new App.Calculator();
-calc.getRandomNumber();
 
 const appDiv: HTMLElement = document.getElementById('app');
 
+// Controlling code
+let val = calc.addPure(1, 1);
+val = calc.addPure(5, val);
+val = calc.addPure(7, val);
+
 // Pure Function Example
-appDiv.innerHTML = calc.addPure(1, 1) as any;
-
-// Impure Function Example
-calc.setState(5);
-appDiv.innerHTML = calc.addImpure(1, 1) as any;
-
-// Impure Function calling http
-setTimeout(() => {
-    appDiv.innerHTML = calc.addImpure(1, 1) as any;
-}, 2000);
+appDiv.innerHTML = val as any;
 
