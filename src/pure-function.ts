bfa2aeb8944af7c68b4e06a34f2c5dee9cf0d61a
tomfriedhof <tom@activelamp.com>
2020-01-20 12:20:13 -0700

    Initial commit

---
+++ src/pure-function.ts
@@ -0,0 +1,23 @@
+const request = require('request-promise-native');
+
+export class Calculator {
+  private state: number;
+
+  addPure(a: number, b: number): number {
+    return a + b;
+  }
+
+  addImpure (a:number, b:number): number {
+    return this.state + a + b;
+  }
+
+  setState(a:number) {
+    this.state = a;
+  }
+
+  async getRandomNumber() {
+    const randomNumber = await request('http://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new');
+    this.state = +randomNumber;
+  }
+
+}
