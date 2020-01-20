0b54eeb4f7090214dff39b08151c201347d291d5
Tom Friedhof <tom@activelamp.com>
2020-01-20 15:41:42 -0700

    two observables with combinelatest

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { clickEvents$, seconds$} from "./observable";
+import { combineLatest } from "rxjs";
 
 const appDiv: HTMLElement = document.getElementById('app');
 
-seconds$.subscribe((val: any) => {
-    appDiv.innerHTML = val;
-});
+// seconds$.subscribe((val: any) => {
+//     appDiv.innerHTML = val;
+// });
+//
+// clickEvents$.subscribe((val: any) => {
+//     appDiv.innerHTML = `${val.screenX}, ${val.screenY}`;
+// });
 
-clickEvents$.subscribe((val: any) => {
-    appDiv.innerHTML = `${val.screenX}, ${val.screenY}`;
-});
-
+const result$ = combineLatest(seconds$, clickEvents$);
+result$.subscribe(([seconds, clicks]: [number, MouseEvent]) => {
+    appDiv.innerHTML = `<h2>Seconds: ${seconds}</h2><p>${clicks.screenX}, ${clicks.screenY}</p>`;
+})
